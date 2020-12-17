d76ab59b36e340b9ceb25aafd7104b85318057b8
chester <374488168@qq.com>
2020-12-18 00:05:49 +0800

    Add back button to edit page

--- hexo-admin/src/components/posts/PostEdit.js
+++ hexo-admin/src/components/posts/PostEdit.js
@@ -1,9 +1,20 @@
-import { Edit, SimpleForm } from "react-admin";
+import { Edit, ListButton, SimpleForm, TopToolbar } from "react-admin";
 import PostTitle from "./PostTitle";
 import MdEditor from "../common/MdEditor";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+
+const PostEditActions = ({ basePath, data }) => (
+  <TopToolbar>
+    <ListButton basePath={basePath} label="Back" icon={<ChevronLeft />} />
+  </TopToolbar>
+);
 
 const PostEdit = (props) => (
-  <Edit title={<PostTitle source="title" />} {...props}>
+  <Edit
+    title={<PostTitle source="title" />}
+    actions={<PostEditActions />}
+    {...props}
+  >
     <SimpleForm>
       <MdEditor source="_content" />
     </SimpleForm>
